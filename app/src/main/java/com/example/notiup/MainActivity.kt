f00dd0b04948ab9044caeb014d7e99b3eb85b148
chaeyoung Moon <80873608+moon12-3@users.noreply.github.com>
2023-02-26 23:11:04 +0900

    month 및 alarm 합침

--- app/src/main/java/com/example/notiup/MainActivity.kt
+++ app/src/main/java/com/example/notiup/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.notiup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notiup.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction().add(R.id.frame, MonthFragment()).commit()
+
+        val navView2 = findViewById<BottomNavigationView>(R.id.navigationView)
+
+        navView2.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.article_month -> supportFragmentManager.beginTransaction().replace(R.id.frame, MonthFragment()).commit()
+                R.id.article_alarm -> supportFragmentManager.beginTransaction().replace(R.id.frame, AlarmFragment()).commit()
+            }
+
+            true
+        }
     }
 }
