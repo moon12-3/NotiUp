d9124351e9752fbda333b47464ce1b815d8cbd40
cuteNK <zzum0701@naver.com>
2023-02-28 19:04:59 +0900

    AlarmFragment 설정, floating 버튼

--- app/src/main/java/com/example/notiup/AlarmFragment.kt
+++ app/src/main/java/com/example/notiup/AlarmFragment.kt
@@ -6,20 +6,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Switch
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notiup.databinding.FragmentAlarmBinding
 
 class AlarmFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-    }
+    lateinit var binding : FragmentAlarmBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm, container, false)
+        val view = inflater.inflate(R.layout.fragment_alarm, container, false)
+        val alarmList = arrayListOf(
+            Alarms("08 : 30", "여기에 알람 문구가 뜹니다", "노랑"),
+            Alarms("07 : 30", "aaaaaa", "노랑"),
+            Alarms("12 : 20", "ddd", "노랑"),
+            Alarms("08 : 30", "여기에 알람 문구가 뜹니다", "노랑"),
+            Alarms("08 : 30", "여기에 알람 문구가 뜹니다", "노랑"),
+            Alarms("08 : 30", "여기에 알람 문구가 뜹니다", "노랑")
+
+        )
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_alarm)
+
+        // 레이아웃매니저 설정
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.setHasFixedSize(true)
+
+        // 어댑터 설정
+        recyclerView.adapter = AlarmAdapter(alarmList)
+
+        return view
     }
 }
