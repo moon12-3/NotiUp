39c61ac5571814c1f33fd45b7e179abad829818c
cuteNK <zzum0701@naver.com>
2023-04-03 20:31:21 +0900

    알람 제목 추가

--- app/src/main/java/com/example/notiup/BottomSheet.kt
+++ app/src/main/java/com/example/notiup/BottomSheet.kt
@@ -1,9 +1,13 @@
 package com.example.notiup
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
 
@@ -16,6 +20,31 @@ class BottomSheet : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.bottom_sheet, container, false)
+
+        val etMemo = view.findViewById<EditText>(R.id.et_memo)
+        etMemo.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (etMemo.lineCount > 2) {
+                    etMemo.text.delete(etMemo.selectionStart - 1, etMemo.selectionStart)
+                }
+                if (s?.toString()?.contains("\n") == true) {
+                    etMemo.focusSearch(View.FOCUS_DOWN)?.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        etMemo.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                etMemo.clearFocus()
+                true
+            } else {
+                false
+            }
+        }
 
         val list: ArrayList<DropdownList> = ArrayList()
 
